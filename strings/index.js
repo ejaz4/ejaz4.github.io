ec9656c04a27aa6d2d9960d08ce759924a8e77ee
Ejaz <ejazali4@protonmail.ch>
2021-10-02 01:04:13 +0100

    💄

---
+++ strings/index.js
@@ -0,0 +1,25 @@
+flsestrings["helloim"] = {
+    default: "Hey, I'm <a style='text-decoration: underline'>Ejaz</a>.<br>A full-stack developer.",
+    ja: "こんにちは、<a style='text-decoration: underline'>Ejaz</a>です。<br>私はプログラマーです。",
+    fr: "Salut, je suis <a style='text-decoration: underline'>Ejaz</a>.<br>Un développeur professionnel."
+}
+flsestrings["download-resume"] = {
+    default: "Download Resume",
+    en_GB: "Download CV",
+    ja: "ダウンロード履歴書",
+    fr: "Télécharger le CV"
+}
+
+flsestrings["ibuildsoftware"] = {
+    default: "I build software.",
+    en_GB: "I build platforms.",
+    ja: "私はソフトウェアを構築します。",
+    fr: "Je construis des programmes."
+}
+
+flsestrings["ihavexp"] = {
+    default: "I have experience with the following tools:",
+    ja: "私は以前にこれらを使用しました",
+    fr: "Je les ai déjà utilisés:"
+
+}
